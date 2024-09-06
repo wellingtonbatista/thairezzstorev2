2dff42a0b49abad4ed786a4aac0be851bf87c53c
wellington <wellington.batista.souza@hotmail.com>
2024-09-06 00:22:43 -0300

    Remocao de produtos pedidos de compra + cadastro movimentacao de estoque

--- app/Livewire/Entradas/DetailsEntrada.php
+++ app/Livewire/Entradas/DetailsEntrada.php
@@ -23,9 +23,16 @@ class DetailsEntrada extends Component
     public $produtos = [];
     public $depositos = [];
     public $entrada;
+    public $natureza_operacao = [];
 
     // MODAL
     public $myModal1 = false;
+    public $myModal2 = false;
+
+    // DADOS PARA REMOVER PRODUTOS DE ENTRADAS_PRODUTOS
+    public $id_entrada_produto_remover;
+    public $id_natureza_operacao_remover;
+    public $id_depositos_remover;
 
     // DADOS DE EXIBICAO DE INFORMAÇÃO
     public $valor_total_pedido;
@@ -48,27 +55,38 @@ class DetailsEntrada extends Component
         $this->entrada = Entradas::find($this->id_entrada);
         $this->produtos = Produto::all();
         $this->depositos = Deposito::all();
+        $this->natureza_operacao = NaturezaOperacao::where('tipo_movimentacao', 1)->get();
     }
 
     public function create_produto_entrada()
     {
-        /*
         EntradasProdutos::create([
             'entrada_id' => $this->id_entrada,
             'produto_id' => $this->id_produto_entrada,
             'valor_compra' => $this->valor_compra_entrada,
             'quantidade' => $this->quantidade_entrada
         ]);
-        */
 
         // MOVIMENTAÇÃO DE ESTOQUE
         Estoque::create([
             'id_produto' => $this->id_produto_entrada,
             'id_natureza_operacao' => $this->entrada->natureza_operacao->id,
-            'id_deposito' => '',
+            'id_deposito' => $this->id_deposito_entrada,
             'quantidade' => $this->quantidade_entrada
         ]);
 
+        // ADICIONAR ESTOQUE AO PRODUTO
+        $produto = Produto::find($this->id_produto_entrada);
+
+        if($this->entrada->natureza_operacao->bonificacao)
+        {
+            $produto->estoque_bonificacao = $produto->estoque_bonificacao + $this->quantidade_entrada; 
+        } else {
+            $produto->estoque = $produto->estoque + $this->quantidade_entrada;
+        }
+
+        $produto->save();
+        
 
         $this->reset(
             'id_produto_entrada',
@@ -86,9 +104,33 @@ class DetailsEntrada extends Component
         );
     }
 
-    public function remove_produto_entrada($id)
+    public function remove_produto_entrada()
     {
-        EntradasProdutos::find($id)->delete();
+        $entrada_produto = EntradasProdutos::find($this->id_entrada_produto_remover);
+
+        // MOVIMENTACAO DE ESTOQUE
+        Estoque::create([
+            'id_produto' => $entrada_produto->produto_id,
+            'id_natureza_operacao' => $this->id_natureza_operacao_remover,
+            'id_deposito' => $this->id_depositos_remover,
+            'quantidade' => $entrada_produto->quantidade
+        ]);
+
+        // REMOVENDO ESTOQUE PRODUTO
+        $produto = Produto::find($entrada_produto->produto_id);
+
+        if($this->entrada->natureza_operacao->bonificacao)
+        {
+            $produto->estoque_bonificacao = $produto->estoque_bonificacao - $entrada_produto->pivot->quantidade;
+        } else {
+            $produto->estoque = $produto->estoque - $entrada_produto->quantidade;
+        }
+
+        $produto->save();
+
+        $entrada_produto->delete();
+
+        $this->myModal2 = false;
 
         $this->toast(
             type: 'error',
