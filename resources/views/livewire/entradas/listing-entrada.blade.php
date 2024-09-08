3e0f90b628b435806bcdff72d1a767c2fff5f611
wellington <wellington.batista.souza@hotmail.com>
2024-09-08 00:07:58 -0300

    Menu de opcoes na listagem de produtos

--- resources/views/livewire/entradas/listing-entrada.blade.php
+++ resources/views/livewire/entradas/listing-entrada.blade.php
@@ -19,19 +19,34 @@
                             <th class="text-sm font-bold text-black">Fornecedor:</th>
                             <td class="text-sm font-bold text-black text-center">Data de Compra:</td>
                             <td class="text-sm font-bold text-black text-center">Natureza de Operação</td>
-                            <td class="text-sm font-bold text-black text-center">Valor Total</td>
                             <td class="text-sm font-bold text-black text-end">Detalhes</td>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($entradas as $entrada)
                             <tr>
-                                <th>{{ $entrada->fornecedor->nome }}</th>
-                                <td class="text-center">{{ date('d/m/Y', strtotime($entrada->data_entrada)) }}</td>
-                                <td class="text-center">{{ $entrada->natureza_operacao->nome }}</td>
-                                <td></td>
-                                <td class="text-end">
-                                    <a href="{{ route('entrada.details', ['id_entrada' => $entrada->id]) }}" wire:navigate class="btn-success"><i class="bi bi-view-list"></i></a>
+                                <th class="py-4">{{ $entrada->fornecedor->nome }}</th>
+                                <td class="text-center py-4">{{ date('d/m/Y', strtotime($entrada->data_entrada)) }}</td>
+                                <td class="text-center py-4">{{ $entrada->natureza_operacao->nome }}</td>
+                                <td class="text-end py-4">
+                                    <div class="dropdown">
+                                        <div tabindex="0" role="button" class="btn-warning m-1"><i class="bi bi-view-list"></i></div>
+                                        <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                                          <li>
+                                            <a href="{{ route('entrada.details', ['id_entrada' => $entrada->id]) }}" wire:navigate>Detalhes</a>
+                                          </li>
+                                          <li>
+                                            @if ($entrada->estoque_lancado)
+                                                <button wire:click="ExtornarEstoque({{$entrada->id}})">Extornar Estoque</button>
+                                            @else
+                                                <button wire:click="LancarEstoque({{$entrada->id}})">Lançar Estoque</button>
+                                            @endif
+                                          </li>
+                                          <li>
+                                            <button wire:click="ArquivarPedido({{$entrada->id}})">Arquivar</button>
+                                          </li>
+                                        </ul>
+                                    </div>
                                 </td>
                             </tr>
                         @endforeach
