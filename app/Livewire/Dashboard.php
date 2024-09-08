c425547448b630d7d56f4cbce921066955475d27
wellington <wellington.batista.souza@hotmail.com>
2024-09-08 00:05:06 -0300

    Atualizacao de metodos de calculos informacoes dashboard

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\EntradasProdutos;
 use App\Models\Produto;
 use Livewire\Component;
 
@@ -20,10 +21,24 @@ class Dashboard extends Component
     public function mount()
     {
         $produtos = Produto::all();
+        $entradas = EntradasProdutos::all();
 
         //QUANTIDADE TOTAL DE PRODUTOS CADASTRADOS
         $this->produtos_cadastrados = $produtos->count();
 
-        
+        // CALCULAR VALOR TOTAL DO ESTOQUE
+        foreach($produtos as $produto)
+        {
+            $this->valor_total_estoque = $this->valor_total_estoque + ($produto->valor_venda * $produto->estoque);
+        }
+
+        // CALCULAR VALOR TOTAL INVESTIDO
+        foreach($entradas as $entrada)
+        {
+            $this->valot_total_investido = $this->valot_total_investido + ($entrada->valor_compra * $entrada->quantidade);
+        }
+
+        // CALCULAR LUCRO TOTAL
+        $this->lucro_total_estoque = $this->valor_total_estoque - $this->valot_total_investido;
     }
 }
